b91ffdd48049419fd6b3098c0ecd032de21fe010
felixliu <felixliu@umich.edu>
2022-09-10 15:59:33 -0400

    added comments

--- block.h
+++ block.h
@@ -11,6 +11,8 @@ namespace block_chain
 
     static const int PREFIX_ZERO_COUNT = 3;
 
+    // the Block class holds the important fields of a block, 
+    // including data, timestamp, hash, previous hash, salt, and signature.
     class Block
     {
     private:
@@ -44,10 +46,20 @@ namespace block_chain
         void set_hash(string hash) { this->hash = hash; }
         void set_prev_hash(string hash) { this->prev_hash = hash; }
         void mining();
+
+        // hash calculation method using md5 (16 byte)
         string calculate_hash_md5();
+
+        // hash calculation method using sha256 (32 byte)
         string calculate_hash_sha256();
+
+        // checks validity of this block
         bool is_valid();
+
+        // digital signing block using private key
         void sign();
+
+        // verifies digital signature using public key
         bool verify_signature();
     };
 };
--- block_chain.h
+++ block_chain.h
@@ -9,11 +9,17 @@ namespace block_chain
 {
     using namespace std;
 
+    // BlockChain class represents chain of blocks. Major functions to create, verify, and validate blocks
     class BlockChain
     {
     public:
+        // creates new block with specified data at the end of the chain
         void create_block(string data);
+
+        // verifies a specific block on the chain
         bool verify_block(Block & block);
+
+        // verifies all blocks in the whole chain
         bool validate_blocks();
         Block get_block(int index);
         BlockChain() {}
